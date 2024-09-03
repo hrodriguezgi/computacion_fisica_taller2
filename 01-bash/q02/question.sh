f4bb9da9bd3d8c571e1ba7af9d30fa932ece1075
Valeria Giraldo <vale.sgir@gmail.com>
2024-09-03 07:42:36 -0500

    Eliminado taller 1

--- 01-bash/q02/question.sh
+++
@@ -1,9 +0,0 @@
-##
-##  Gestion de datos con BASH
-##  ===========================================================================
-##
-##  Cual es la 'ciudad (estado)' en la posicion 2 del archivo 'person', si el 
-##  archivo se organiza alfabeticamente por el campo 'ssn'?
-## 
-##  >>> Escriba su codigo a partir de este punto <<<
-sort person | head -n 2 | tail -n 1 | cut -d ',' -f3
--- 01-bash/q09/question.sh
+++
@@ -1,8 +0,0 @@
-##
-##  Gestion de datos con BASH
-##  ===========================================================================
-##
-##  Cuantas veces aparece el número 1192 en el segundo grupo de digitos de las 
-##  tarjetas de  credito?
-## 
-##  >>> Escriba su codigo a partir de este punto <<<<
